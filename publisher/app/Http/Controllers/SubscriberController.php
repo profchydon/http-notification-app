c42a2c6f001c68782986c1976a6babdcd2361f33
Chidi <profchydon@gmail.com>
2021-03-28 19:26:03 +0100

    Add comment for Subscriber Controller

--- publisher/app/Http/Controllers/SubscriberController.php
+++ publisher/app/Http/Controllers/SubscriberController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 use App\Http\Repository\SubscriberRepository;
 
@@ -12,19 +13,47 @@ class SubscriberController extends Controller
 
     private $subscriber;
 
+    /**
+     * Initialize a new instance of SubscriberRepository
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Http\Repository\SubscriberRepository
+     * @return $subscriber
+     */
     public function __construct(SubscriberRepository $subscriber)
     {
         $this->subscriber = $subscriber;
     }
     
-    //
+    /**
+     * Create a subscription.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string $topic
+     * @return Response
+     */
     public function createSubscription($topic, Request $request)
     {
 
+        // VALIDATE INPUT DATA
+        $validator = Validator::make($request->all(), [
+
+            'url' => 'required',
+
+        ]);
+
+        if($validator->fails()){
+
+            return $this->sendError('Validation Error.', $validator->errors());
+
+        }
+
         $url_array = [];
 
+        // GET ALL SUBSCRIBERS FOR $TOPIC FROM REDIS 
         $subscribers = $this->subscriber->getFromRedis($topic);
 
+        // CHECK IF SUBSCRIBER $request->url HAS BEEN SUBSCRIBED TO TOPIC $topic
         if (in_array($request->url, (array) $subscribers)) {
 
             $url = $request->url;
@@ -38,6 +67,7 @@ class SubscriberController extends Controller
 
         $data = $url_array ? $url_array : $subscribers;
 
+        // STORE DATA TO REDIS
         $this->subscriber->setToRedis($topic, $data);
 
         $response = [
